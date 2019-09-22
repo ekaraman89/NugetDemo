26d1cdcc6848e63fbcaf77f81efdbf1b40b440f4
emrah karaman <ekaraman89@hotmail.com>
2019-09-22 19:16:45 +0300

    nuget test

---
+++ NugetDemo/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetDemo
+{
+    class MyClass
+    {
+        async Task DownloadAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            await httpClient.GetStringAsync("");
+
+        }
+    }
+}
